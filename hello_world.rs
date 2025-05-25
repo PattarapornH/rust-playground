f129fea03545d38d8bc78c444bba1c4ead9a2bf1
Pattaraporn Hanphadungkit <pataraporn.han@gmail.com>
2025-05-25 18:18:45 +0700

    Add: hello world rust

---
+++ hello_world.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    println!("{}", "Hello, world! from fomatted string");
+    println!("{0} {1}", "Hello, world!", "from positional fomatted string");
+    println!("{x} {y}", x="Hello, world!", y="from named formatted string");
+
+}
